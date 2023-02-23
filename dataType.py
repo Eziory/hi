4256be25e5eddd246ea6db4de805c2750dca147a
Ezio <2429666204@qq.com>
2023-02-23 20:53:51 +1100

    add new file

---
+++ dataType.py
@@ -0,0 +1,23 @@
+# data types（数据类型）
+# 在Python中又或是其他的代码语言中都有4中关键的数据类型
+# 1. 整数（integer）int
+# 2. 浮点数（float）float
+# 3. 布林值（boolean）bool（只有True和False这两个值）
+# 4. 字串符（string）str 可以想象成文字部分，所有的字串都有'，比如'Ezio'
+
+# 那么接下来我们来declare四个变量测试一下
+price = 10
+# 这里price就是integer
+pen = 0.38
+# 这里的pen就是float
+rain = True
+# 这里的rain就是boolean
+name = 'Ezio'
+# 这里的name就是string，别忘记加''
+print(price)
+print(pen)
+print(rain)
+print(name)
+
+# 我们也可以用,在一个print中印出很多东西
+print(price, pen, rain, name)
